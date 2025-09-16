0fb801599326d63830c3009065b83d402f0dd1b5
Dee <aeinhmu.naychi@sjsu.edu>
2025-09-15 21:46:47 -0700

    Add the abstract Animal Class

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+
+
+public abstract class Animal {
+
+    public void makeSound() {
+        System.out.println("animal makes sound!");
+    }
+
+    //abstract method
+    public abstract void getDiet(); 
+
+}
