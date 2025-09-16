73f547c3afd97245fbc875f5a4df728780ce2343
Dee <aeinhmu.naychi@sjsu.edu>
2025-09-15 22:27:32 -0700

    Implement Lion and Parrot classes

---
+++ src/Lion.java
@@ -0,0 +1,15 @@
+
+
+public class Lion extends Mammal {
+
+    @Override 
+    public void makeSound() {
+        System.out.println("Roar");
+    }
+
+    @Override
+    public void getDiet() {
+        System.out.println("Carnivore");
+    }
+
+}
